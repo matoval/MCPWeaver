bd2f36438be829b91c9229bdd5f17c4e9d092fce
matoval <mat24c@gmail.com>
2025-07-17 13:41:39 -0700

    [Phase 1] Wails Context API Implementation

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"embed"
 
+	"MCPWeaver/internal/app"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -13,20 +15,33 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	application := app.NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "MCPWeaver",
-		Width:  1024,
-		Height: 768,
+		Title:            "MCPWeaver",
+		Width:            1200,
+		Height:           800,
+		MinWidth:         800,
+		MinHeight:        600,
+		MaxWidth:         0,
+		MaxHeight:        0,
+		DisableResize:    false,
+		Fullscreen:       false,
+		Frameless:        false,
+		StartHidden:      false,
+		HideWindowOnClose: false,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup:        func(ctx context.Context) { application.OnStartup(ctx) },
+		OnDomReady:       application.OnDomReady,
+		OnBeforeClose:    application.OnBeforeClose,
+		OnShutdown:       func(ctx context.Context) { application.OnShutdown(ctx) },
+		WindowStartState: options.Normal,
 		Bind: []interface{}{
-			app,
+			application,
 		},
 	})
 
